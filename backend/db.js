65b275311b73ac95f65cfca9a11c142c6115bfdc
Yash Raj <yash_2001ee83@iitp.ac.in>
2023-01-12 23:35:57 +0530

    Initial Commit

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const server = '127.0.0.1:27017';
+const database = 'Test';
+
+const mongoURI =
+  "mongodb://localhost:27017/?directConnection=true";
+
+const connectToMongo = async () => {
+//   mongoose.set("strictQuery", false);
+    try{
+        await mongoose.connect(`mongodb://${server}/${database}`);
+        console.log("Connected to MongoDB");
+    }
+    catch (err)
+    {
+        console.log("Failed to connect to MongoDB", err);
+    }
+}
+
+
+module.exports = connectToMongo;
